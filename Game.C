a04b35e582b52a1357b8846edb484518d7a2f973
Kai-Wen-Xu <you@example.com>
2025-03-29 20:40:31 +0800

    Finished F=P-(UNO U TRES)

--- Game.C
+++ Game.C
@@ -1,37 +1,65 @@
 #include <stdio.h>
-#include <stdbool.h>
+#include <string.h>
 
 #define MAX_POSITIONS 16 // Max area from (A x A) where A = {1,2,3,4}
 
 // Initialize the system
-void initialize_system(int uno_size, int dos_size, int tres_size, int turn, int go) {
-    uno_size = dos_size = tres_size = 0; // Ensure sets are empty
-    turn = 1; 
-    go = 0; 
+void initialize_system(int Uno[][2], int Dos[][2], int Tres[][2], int* turn, int* go) {
+     // Ensure sets are empty
+	for (int i=0; i<MAX_POSITIONS; i++) {
+        Uno[i][0] = -1;
+        Uno[i][1] = -1;
+        Dos[i][0] = -1;
+        Dos[i][1] = -1;
+        Tres[i][0] = -1;
+        Tres[i][1] = -1;
+    }
+	
+    *turn=1; 
+    *go=0; 
     
     printf("System initialized.\n");
     printf("Uno, Dos, Tres are empty.\n");
-    printf("turn = %s\n", "true");
-    printf("go = %s\n", "false");
+    if(*turn)
+		printf("turn = true\n");
+	else
+		printf("turn = false\n");
+
+	if(*go)
+		printf("go = true\n");
+	else
+		printf("go = false\n");
+}
+
+//Calculate Size
+int calculate_size(int Set[][2]){
+    int i, count=0;
+	
+    for (i=0; i<MAX_POSITIONS; i++) {
+        if (Set[i][0]!=-1&&Set[i][1]!=-1){
+            count++;
+        }
+    }
+    return count;
 }
 
 //Uno U Tres
 int
-UnoUnionTres(int Uno[][2], int Tres[][2], int Union[][2], int UNO, int TRES){
+uno_union_tres(int Uno[][2], int Tres[][2], int Union[][2], int uno_size, int tres_size){
 	int i=0, j=0, k=0, UNION=0, found=0;
 	
 	//add everything in uno to union
-	for(i=0; i<UNO; i++){
+	for(i=0; i<uno_size; i++){
 		Union[UNION][0]=Uno[i][0];
 		Union[UNION][1]=Uno[i][1];
 		UNION++;
 	}
 	
 	//add elements of tres to union if not in uno
-	for(j=0; j<TRES; j++){
+	for(j=0; j<tres_size; j++){
 		found=0;//assume element is not in uno
 		
-		for(k=0; k<UNO; k++){
+		for(k=0; k<uno_size; k++){
 			if(Uno[k][0]==Tres[j][0]&&Uno[k][1]==Tres[j][1])
 				found=1;
 		}
@@ -47,8 +75,14 @@ UnoUnionTres(int Uno[][2], int Tres[][2], int Union[][2], int UNO, int TRES){
 }
 
 int
-PMinusUnion(int P[][2], int Union[][2], int F[][2], int UNO){
+UpdateF(int P[][2], int Uno[][2], int Tres[][2], int F[][2]){
 	int i=0, j=0, k=0, index=0, found=0, shift=0;
+	int union_size, uno_size, tres_size;
+	int Union[2*MAX_POSITIONS][2];
+	
+	uno_size=calculate_size(Uno);
+	tres_size=calculate_size(Tres);
+	union_size=uno_union_tres(Uno, Tres, Union, uno_size, tres_size);//Uno U Tres
 	
 	//store everything in P to F
 	for(i=0; i<MAX_POSITIONS; i++){
@@ -61,7 +95,7 @@ PMinusUnion(int P[][2], int Union[][2], int F[][2], int UNO){
 	for(j=0; j<index; j++){
 		found=0;
 		
-		for(k=0; k<UNO; k++){
+		for(k=0; k<union_size; k++){
 			if(F[j][0]==Union[k][0]&&F[j][1]==Union[k][1])
 				found=1;
 		}
@@ -96,22 +130,19 @@ int main() {
 	int T[4][2]={{1,1}, {2,2}, {3,3}, {4,4}};
 	
 	//System variables
-	int uno_size, dos_size, tres_size;
 	int turn, go, over=0;
-	
-	initialize_system(uno_size, dos_size, tres_size, turn, go);
-	
-	int Uno[uno_size][2];
-	int Dos[dos_size][2];
-	int Tres[tres_size][2];
+	int Uno[MAX_POSITIONS][2];
+	int Dos[MAX_POSITIONS][2];
+	int Tres[MAX_POSITIONS][2];
 	int F[MAX_POSITIONS][2];
 	
+	//System initialization
+	initialize_system(Uno, Dos, Tres, &turn, &go);
+	
 	//Sytem Facts
-	int Union[uno_size+tres_size][2];
-	int union_size;
-	union_size=UnoUnionTres(Uno, Tres, Union, uno_size, tres_size);//Uno U Tres
 	int F_size;
-	F_size=PMinusUnion(P, Union, F, uno_size); //F=P-(Uno U Tres)
+	F_size=UpdateF(P, Uno, Tres, F); //F=P-(Uno U Tres)
+	//W=C-T
 	
     return 0;
 }
